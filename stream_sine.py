8c5f88013422b214a4eb965d8f36f7a69ab13526
Stéphane Caron <stephane.caron@inria.fr>
2024-05-09 11:51:09 +0200

    Update working example

--- stream_sine.py
+++ stream_sine.py
@@ -8,31 +8,31 @@ import time
 
 import numpy as np
 
-from matplotlive import LivePlot
+from matplotlive import Sketch
 
 t_min = 0.0
 t_max = 10.0
 nb_knots = 12
 trange = np.linspace(t_min, t_max, nb_knots)
 
-live_plot = LivePlot(
+sketch = Sketch(
     xlim=(t_min, t_max),
     ylim=(-1.5, 1.5),
-    ylim_rhs=(-3.5, 3.5),
+    ylim_right=(-3.5, 3.5),
 )
 
-live_plot.add_line("sine", "b-")
-live_plot.left_axis.set_ylabel("sin(t)", color="b")
-live_plot.left_axis.tick_params(axis="y", labelcolor="b")
+sketch.add_line("sine", "left", "b-")
+sketch.left_axis.set_ylabel("sin(t)", color="b")
+sketch.left_axis.tick_params(axis="y", labelcolor="b")
 
-live_plot.add_line_right("cosine", "g-")
-live_plot.right_axis.set_ylabel("3 cos(t)", color="g")
-live_plot.right_axis.tick_params(axis="y", labelcolor="g")
+sketch.add_line("cosine", "right", "g-")
+sketch.right_axis.set_ylabel("3 cos(t)", color="g")
+sketch.right_axis.tick_params(axis="y", labelcolor="g")
 
 dt = 1e-2
 for i in range(100):
     t = i * dt
-    live_plot.update_line("sine", trange, np.sin(trange + t))
-    live_plot.update_line("cosine", trange, 3 * np.cos(trange + t))
-    live_plot.update()
+    sketch.update_line("sine", trange, np.sin(trange + t))
+    sketch.update_line("cosine", trange, 3 * np.cos(trange + t))
+    sketch.update()
     time.sleep(dt)
