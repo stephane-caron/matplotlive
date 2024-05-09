4b33729d9134443b12eb9b650ab0a002313aecc9
Stéphane Caron <stephane.caron@inria.fr>
2024-05-09 12:22:42 +0200

    Implement RecentPass plot

--- matplotlive/recent_past.py
+++ matplotlive/recent_past.py
@@ -6,8 +6,13 @@
 
 """Live plot to which time series data is streamed."""
 
-from typing import Optional, Tuple
+from typing import Dict, Optional, Tuple
 
+import matplotlib
+import numpy as np
+from numpy.typing import NDArray
+
+from .exceptions import MatplotliveError
 from .sketch import Sketch
 
 
@@ -15,10 +20,12 @@ class RecentPast:
     """Live plot to which time series data is streamed."""
 
     sketch: Sketch
+    trange: NDArray[np.float64]
 
     def __init__(
         self,
         duration: float,
+        timestep: float,
         ylim: Tuple[float, float],
         ylim_right: Optional[Tuple[float, float]] = None,
         faster: bool = True,
@@ -27,12 +34,36 @@ class RecentPast:
 
         Args:
             duration: Duration of the recent-past sliding window, in seconds.
+            timestep: Time step between two successive values, in seconds.
             ylim: Limits for left y-axis.
             ylim_right: If set, create a right y-axis with these limits.
             faster: If set, use blitting.
         """
         xlim = (-abs(duration), 0.0)
+        trange = np.flip(-np.arange(0.0, duration, timestep))
+        self.__max_updates: int = 0
+        self.__nb_updates: Dict[str, int] = {}
+        self.__shape = (len(trange),)
+        self.series: Dict[str, NDArray[np.float64]] = {}
         self.sketch = Sketch(xlim, ylim, ylim_right, faster)
+        self.trange = trange
+
+    @property
+    def left_axis(self) -> matplotlib.axes._subplots.Axes:
+        """Left axis of the plot."""
+        return self.sketch.left_axis
+
+    @property
+    def right_axis(self) -> matplotlib.axes._subplots.Axes:
+        """Right axis of the plot."""
+        return self.sketch.right_axis
+
+    def __add(self, name: str, side: str, *args, **kwargs) -> None:
+        self.sketch.add_line(name, side, *args, **kwargs)
+        if name in self.series:
+            raise MatplotliveError(f"a series named '{name}' already exists")
+        self.series[name] = np.full(self.__shape, np.nan)
+        self.__nb_updates[name] = 0
 
     def add_left(self, name: str, *args, **kwargs) -> None:
         """Add a new time series to the left axis.
@@ -42,7 +73,7 @@ class RecentPast:
             args: Forwarded to ``pyplot.plot``.
             kwargs: Forwarded to ``pyplot.plot``.
         """
-        self.sketch.add_line(name, "left", *args, **kwargs)
+        self.__add(name, "left", *args, **kwargs)
 
     def add_right(self, name: str, *args, **kwargs) -> None:
         """Add a new time series to the right axis.
@@ -52,7 +83,7 @@ class RecentPast:
             args: Forwarded to ``pyplot.plot``.
             kwargs: Forwarded to ``pyplot.plot``.
         """
-        self.sketch.add_line(name, "right", *args, **kwargs)
+        self.__add(name, "right", *args, **kwargs)
 
     def send(self, name: str, value: float) -> None:
         """Send a new value for a given time series.
@@ -61,4 +92,29 @@ class RecentPast:
             name: Name of the time series.
             value: New value for the series.
         """
-        pass
+        # Deleting and appending is slightly faster than rolling an array of
+        # size 20 (mean ± std. dev. of 7 runs, 100,000 loops each):
+        #
+        #     %timeit np.append(np.delete(a, 0), 11111)
+        #     5.69 µs ± 18.5 ns per loop
+        #
+        #     %timeit np.roll(a, 1)
+        #     6.49 µs ± 17.4 ns per loop
+        #
+        new_series = np.append(np.delete(self.series[name], 0), value)
+        self.sketch.update_line(name, self.trange, new_series)
+        self.series[name] = new_series
+        self.__nb_updates[name] += 1
+        if self.__max_updates < self.__nb_updates[name]:
+            self.__max_updates = self.__nb_updates[name]
+
+    def update(self) -> None:
+        """Update plot with latest time-series values.
+
+        Calling this function will catch up all time series with the most
+        recent one.
+        """
+        for name in self.series:
+            while self.__nb_updates[name] < self.__max_updates:
+                self.send(name, self.series[name][-1])
+        self.sketch.update()
