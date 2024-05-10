7359e87cdfdef779b498d4337d8cc35777da526d
Stéphane Caron <stephane.caron@inria.fr>
2024-05-10 13:33:43 +0200

    Simplify sine wave example

--- examples/sine_waves.py
+++ examples/sine_waves.py
@@ -6,17 +6,17 @@
 
 """Draw sine waves using a RecentPast plot."""
 
-import time
+import math
 
-import numpy as np
 from loop_rate_limiters import RateLimiter
 
 from matplotlive import RecentPast
 
-omega = 20.0  # Hz
-dt = 1e-2  # [s]
+OMEGA = 20.0  # Hz
+TIMESTEP = 1e-2  # [s]
+
 plot = RecentPast(
-    timestep=dt,
+    timestep=TIMESTEP,
     duration=1.0,
     ylim=(-1.5, 1.5),
     ylim_right=(-3.5, 3.5),
@@ -31,10 +31,10 @@ plot.right_axis.set_ylabel(r"$3 \cos(\omega t)$", color="g")
 plot.right_axis.tick_params(axis="y", labelcolor="g")
 plot.redraw()
 
-rate = RateLimiter(frequency=1.0 / dt)
+rate = RateLimiter(frequency=1.0 / TIMESTEP)
 for i in range(1000):
-    t = i * dt
-    plot.send("sin", np.sin(omega * t))
-    plot.send("3cos", 3 * np.cos(omega * t))
+    t = i * TIMESTEP
+    plot.send("sin", math.sin(OMEGA * t))
+    plot.send("3cos", 3 * math.cos(OMEGA * t))
     plot.update()
     rate.sleep()
