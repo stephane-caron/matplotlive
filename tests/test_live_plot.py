2e545ac213caa569b8eff58ea370690b8422b94a
Stéphane Caron <stephane.caron@inria.fr>
2024-08-06 17:03:36 +0200

    Fix headless unit tests

--- tests/test_live_plot.py
+++ tests/test_live_plot.py
@@ -14,22 +14,28 @@ from matplotlive import LivePlot, MatplotliveError
 class TestLivePlot(unittest.TestCase):
     """Test live plots."""
 
-    def setUp(self):
-        self.plot = LivePlot(
-            timestep=0.01,
-            duration=0.5,
-            ylim=(-1.0, 1.0),
+    def make_test_plot(self, *args, **kwargs):
+        return LivePlot(
+            timestep=0.01, duration=0.5, ylim=(-1.0, 1.0), faster=False, *args, **kwargs
         )
 
     def test_init(self):
-        self.assertIsNotNone(self.plot.trange)
-        self.assertEqual(len(self.plot.series), 0)
+        plot = self.make_test_plot()
+        self.assertIsNotNone(plot.trange)
+        self.assertEqual(len(plot.series), 0)
 
     def test_add(self):
-        self.assertEqual(len(self.plot.series), 0)
-        self.plot.add_left("foo")
-        self.assertEqual(len(self.plot.series), 1)
+        plot = self.make_test_plot()
+        self.assertEqual(len(plot.series), 0)
+        plot.add_left("foo")
+        self.assertEqual(len(plot.series), 1)
 
     def test_uninitialized_right(self):
+        plot = self.make_test_plot()
         with self.assertRaises(MatplotliveError):
-            self.plot.add_right("bar")
+            plot.add_right("bar")
+
+    def test_add_right(self):
+        plot = self.make_test_plot()
+        with self.assertRaises(MatplotliveError):
+            plot.add_right("bar")
